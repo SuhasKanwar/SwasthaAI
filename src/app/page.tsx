56287af01ff8523b59309b15ce8cc0888c785ab8
SuhasKanwar <suhas.kanwar@gmail.com>
2025-06-07 02:43:23 +0530

    Added testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import FeaturesSection from "@/components/FeaturesSection";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center">
       <HeroSection />
       <FeaturesSection />
+      <section className="w-full flex flex-col items-center justify-center mt-50">
+        <h2 className="text-5xl font-bold text-center mb-10 bg-gradient-to-r from-blue-600 to-teal-600 bg-clip-text text-transparent">
+          What Our Users Say
+        </h2>
+        <TestimonialCards />
+      </section>
     </div>
   )
 }
