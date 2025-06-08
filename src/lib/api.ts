b64c40d6de737598d723fa3fa209d420ba6aebe5
SuhasKanwar <suhas.kanwar@gmail.com>
2025-06-09 04:43:05 +0530

    Adding the api util

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_USER_BACKEND_BASE_URL}/api`,
-});
+const userBaseURL = process.env.NEXT_PUBLIC_USER_BACKEND_BASE_URL || 'http://localhost:8050/api';
+const doctorBaseURL = process.env.NEXT_PUBLIC_DOCTOR_BACKEND_BASE_URL || 'http://localhost:9050/api';
 
-api.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("access_token");
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+const userApi = axios.create({ baseURL: userBaseURL });
+const doctorApi = axios.create({ baseURL: doctorBaseURL });
 
-export default api;
+[userApi, doctorApi].forEach((instance) =>
+    instance.interceptors.request.use((config) => {
+        const token = sessionStorage.getItem('access_token');
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    })
+);
+
+export {
+    userApi,
+    doctorApi
+}
